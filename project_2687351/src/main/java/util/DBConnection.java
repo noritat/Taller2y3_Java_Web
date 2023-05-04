5f668c0e1d09d51e0546e5a336c1e65822a11d4d
noritat <128448216+noritat@users.noreply.github.com>
2023-05-04 14:36:17 -0500

    Update DBConnection.java

--- project_2687351/src/main/java/util/DBConnection.java
+++ project_2687351/src/main/java/util/DBConnection.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL=
             "jdbc:mysql://localhost:3306/my_app?serverTimezone=America/Bogota";
-    private static final String USER ="root";
+    private static final String USER ="my_app";
     private static final String PASS="";
     private static BasicDataSource pool;
 
